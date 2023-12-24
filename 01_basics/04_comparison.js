4c5067cc79b6f601a4570f5ab4eb8d12bc9c0ea0
goelashu937 <125305288+goelashu937@users.noreply.github.com>
2023-12-24 18:24:13 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+// console.log(2>1)
+console.log("2"==2) //true
+console.log("2"===2) //false (strict checking occurs in this case)
+console.log("2">1) //true
+console.log("02">1) //true
+
+console.log(null>0) //false
+console.log(null==0) //false
+console.log(null>=0) //true
+
+console.log(undefined==0) //false
+console.log(undefined>0)//false
+console.log(undefined>=0)//false
