4790d2ec175c7bb9fba29d72a4977669f5ea8307
goelashu937 <125305288+goelashu937@users.noreply.github.com>
2023-12-24 17:25:37 +0000

    conversions of datatypes

---
+++ 01_basics/03_datatypeconversion.js
@@ -0,0 +1,18 @@
+// let score="33abc"
+// console.log(typeof score)//string
+// console.log(typeof (score))  //string
+// let valueInNumber=Number(score)   //converts 
+// console.log(typeof valueInNumber)  //number
+// console.log(valueInNumber)  //prints 33 (if 33abc prints NaN)
+
+
+// let isLoggedIn=1 //gives true value
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumber=33
+let stringNumber=String(someNumber)//33 is string
+console.log(stringNumber) //33
+console.log(typeof stringNumber) //string
+
