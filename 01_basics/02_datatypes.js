105f8bf483f675f7f85657f0de1ad569cab4b65c
goelashu937 <125305288+goelashu937@users.noreply.github.com>
2023-12-24 08:50:44 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict";//treat all js as a new version
+// alert(3+3) //We are using nodejs ,not browser
+console.log(3+3);
+console.log("ashu")//code readability matters
+
+let state;
+console.log(typeof state); // undefined
+console.log(typeof null)   // object
