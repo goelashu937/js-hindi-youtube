d795cebeac66eea4b9c0b8c86c905f408d5b3397
goelashu937 <125305288+goelashu937@users.noreply.github.com>
2023-12-24 18:02:42 +0000

    operations

--- 01_basics/03_datatypeconversion.js
+++ 01_basics/03_datatypeconversion.js
@@ -12,7 +12,37 @@
 // console.log(booleanIsLoggedIn)
 
 let someNumber=33
-let stringNumber=String(someNumber)//33 is string
-console.log(stringNumber) //33
-console.log(typeof stringNumber) //string
+// let stringNumber=String(someNumber)//33 is string
+// console.log(stringNumber) //33
+// console.log(typeof stringNumber) //string
 
+/*******************operations*****************8*/
+
+// console.log(3**2)
+// console.log((3+2)*5%3)
+let str1="hello"
+let str2=" ashu"
+console.log(str1+str2)
+
+console.log("1"+ 2)
+console.log(1+ "2")
+console.log("1"+ 2+2)
+console.log(1+ 2+"2")
+console.log((3+2)*5%3)
+
+
+console.log(true)    // true
+// console.log(true+) //invalid
+console.log(+true)  //1   
+
+
+//this method is not used due to redbillity issues
+let num1, num2 ,mum3
+num1=num2=num3=2+2
+console.log(num1)
+console.log(num2)
+console.log(num3)
+
+let gameCounter=100
+++gameCounter                //101
+console.log(gameCounter) 
