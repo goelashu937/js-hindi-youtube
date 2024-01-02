1bcebf86a2370dc81238ab5e01be3dd16c0ffbe2
goelashu937 <125305288+goelashu937@users.noreply.github.com>
2024-01-02 07:45:16 +0000

    02/01/23

---
+++ 01_basics/07_dateandtime.js
@@ -0,0 +1,39 @@
+/*let myDate=new Date()
+// console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+console.log(typeof(myDate))
+*/
+// let myCreatedDate=new Date(2023,0,23)//customize acc to our needs
+// console.log(myCreatedDate.toDateString());
+// let myCreatedDate=new Date(2023,0,23,5,9)
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate=new Date("01-14-2023")
+// console.log(myCreatedDate);
+
+
+////+++++++++time stamping in milliseconds++++++///////
+// let myTimeStamp=Date.now()
+// console.log(myTimeStamp)
+// let myCreatedDate=new Date("01-04-2023")
+// console.log(myCreatedDate.getTime())//1672790400000
+// console.log(myTimeStamp)/////1704180673428
+// console.log(myTimeStamp/1000)//1704180673.428
+// console.log(Math.floor(Date.now()/1000))
+
+let newdate=new Date("05-02-2023")
+console.log(newdate)
+console.log(newdate.getDate())//2
+console.log(newdate.toString.getDay())//2
+console.log(newdate.getHours())//7
+console.log(newdate.getMonth())//0
+console.log(newdate.getFullYear())//2024
+
+
+
+
+
+
+
