e7ac8ab191c48517ea5a4750d104cd667d85903d
goelashu937 <125305288+goelashu937@users.noreply.github.com>
2024-01-08 20:23:41 +0000

    error

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,30 @@
+//basic structure of arrays 
+const myArr=[0,1,2,3,4,5]
+// const myArr1=[0,1,2,3,4,5,"ashu goel"]
+// const arr1=["ashu","anu","papa","mummy"]
+
+// console.log(myArr)
+// console.log(myArr1)
+// console.log(arr1)
+// const myArr2= new Array(1,2,3,4)
+// console.log(myArr2)//[1,2,3,4]
+
+/*+++++push and pop in arrays++++++*/
+// myArr.push(6)     
+// console.log(myArr)
+
+// myArr.pop()
+// console.log(myArr)
+
+// myArr.unshift(0)
+// console.log(myArr)
+
+// myArr.shift(0)
+// console.log(myArr)
+
+// console.log(myArr.includes(9))//checking whether it includes 
+// console.log(myArr.indexOf(20))
+
+const newArr = myArr.join()
+console.log(newArr)
+console.log(typeOf(newArr))
